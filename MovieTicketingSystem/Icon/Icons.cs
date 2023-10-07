4c31ed6a98385157baf7d841a6f32c4b202de597
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-10-07 22:28:04 +0800

    Add icons using Google Material Icon through unit code

---
+++ MovieTicketingSystem/Icon/Icons.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTicketingSystem.Icon;
+
+//Code point in Unicode for the icons
+public class Icons
+{
+    public static string PhoneCallIcon = "\ue0b0";
+    public static string TicketIcon = "\ue638";
+    public static string UserIcon = "\ue7fd";
+}
--- MovieTicketingSystem/MauiProgram.cs
+++ MovieTicketingSystem/MauiProgram.cs
@@ -1,25 +1,29 @@
 ﻿using Microsoft.Extensions.Logging;
+using MovieTicketingSystem.View;
+using MovieTicketingSystem.ViewModel;
 
-namespace MovieTicketingSystem
+namespace MovieTicketingSystem;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("MaterialIcons_Regular.ttf", "MaterialIcon");
+            });
 
+        builder.Services.AddTransient<HomePage>();
+        builder.Services.AddTransient<HomePageViewModel>();
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
