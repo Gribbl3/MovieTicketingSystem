86b19406779f2f04aa7a5d3081785bd40c228d6a
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-10-13 16:30:28 +0800

    Add Byte Array to Image Source Converter.

---
+++ MovieTicketingSystem/Converters/ByteArrayToImageSourceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace MovieTicketingSystem.Converters;
+
+public class ByteArrayToImageSourceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is byte[] byteArray)
+        {
+            return ImageSource.FromStream(() => new MemoryStream(byteArray));
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
